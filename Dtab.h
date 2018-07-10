86184a087f0657388403cb5fdce14c5ee8460cb6
Tadeusz Tomoszek <TadeuszJT@Gmail.com>
2018-07-10 17:49:32 +0100

    better version

--- Dtab.h
+++ Dtab.h
@@ -1,26 +1,26 @@
 #ifndef DTAB_H
 #define DTAB_H
 
-#include <string.h>
+#include <stdlib.h>
 
 #define Dtab(T) struct {T *array; int len, cap;}
 
 #define Dtab_init(d)\
-	((d)->array = malloc(0), (d)->len = 0, (d)->cap = 0, 1)
+	((d)->array = NULL, (d)->len = 0, (d)->cap = 0)
 
 #define Dtab_grow(d, n)\
 	(((d)->len += n) > (d)->cap ?\
-	(d)->array = realloc(\
+	*((void**)(&((d)->array))) = realloc(\
 		(d)->array,\
 		sizeof(*((d)->array))*((d)->cap = (d)->len*2)\
-	) : (d)->array)
+	) : 0, 0)
 
 #define Dtab_shrink(d, n)\
 	(((d)->len -= n) < (d)->cap/4 ?\
-	(d)->array = realloc(\
+	*((void**)(&((d)->array))) = realloc(\
 		(d)->array,\
 		sizeof(*((d)->array))*((d)->cap = (d)->len)\
-	) : (d)->array)
+	) : 0, 0)
 
 #define Dtab_destroy(d)\
 	free((d)->array)
