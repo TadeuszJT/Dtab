ca46fe538cd0a7c7a3f7b293055a15c1933d1ddc
Tadeusz Tomoszek <TadeuszJT@Gmail.com>
2018-07-15 20:04:00 +0100

    improved

--- Dtab.h
+++ Dtab.h
@@ -6,23 +6,35 @@
 #define Dtab(T) struct {T *array; int len, cap;}
 
 #define Dtab_init(d)\
-	((d)->array = NULL, (d)->len = 0, (d)->cap = 0)
+do {\
+	(d)->array = NULL;\
+	(d)->len = 0;\
+	(d)->cap = 0;\
+} while (0)
 
 #define Dtab_grow(d, n)\
-	(((d)->len += n) > (d)->cap ?\
-	*((void**)(&((d)->array))) = realloc(\
-		(d)->array,\
-		sizeof(*((d)->array))*((d)->cap = (d)->len*2)\
-	) : 0, 0)
+do {\
+	if (((d)->len += n) > (d)->cap) {\
+		*(void**)&(d)->array = realloc(\
+			(d)->array,\
+			sizeof(*(d)->array) * ((d)->cap = (d)->len*2)\
+		);\
+	}\
+} while (0)
 
 #define Dtab_shrink(d, n)\
-	(((d)->len -= n) < (d)->cap/4 ?\
-	*((void**)(&((d)->array))) = realloc(\
-		(d)->array,\
-		sizeof(*((d)->array))*((d)->cap = (d)->len)\
-	) : 0, 0)
+do {\
+	if (((d)->len -= n) < (d)->cap*0.25) {\
+		*(void**)&(d)->array = realloc(\
+			(d)->array,\
+			sizeof(*(d)->array) * ((d)->cap = (d)->len)\
+		);\
+	}\
+} while (0)
 
 #define Dtab_destroy(d)\
-	free((d)->array)
+do {\
+	free((d)->array);\
+} while (0)
 
 #endif
